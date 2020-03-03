ad20bf9a06a438a32fba67a7e5d6fe3b3525d170
chaosannals <chaosannals@outlook.com>
2020-03-03 16:20:25 +0800

    字典数组。

--- common.php
+++ common.php
@@ -5,6 +5,34 @@ function timing($symbol, $callback)
     $start = microtime(true);
     $result = $callback();
     $interval = number_format(microtime(true) - $start, 10);
-    echo str_pad($symbol, 30)."{$interval}s" . PHP_EOL;
+    echo str_pad($symbol, 30) . "{$interval}s" . PHP_EOL;
     return $result;
+}
+
+function random_set($length, $min = 1, $max = 100000000)
+{
+    $result = [];
+    while (true) {
+        $k = random_int($min, $max);
+        if (isset($result[$k])) {
+            continue;
+        }
+        $result[$k] = null;
+        if (count($result) >= $length) {
+            break;
+        }
+    }
+    return $result;
+}
+
+function save_data($path, $data)
+{
+    $text = serialize($data);
+    file_put_contents($path, $text);
+}
+
+function load_data($path)
+{
+    $text = file_get_contents($path);
+    return unserialize($text);
 }
---
+++ list_v_dict/operation.php
@@ -0,0 +1,52 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$da = random_set(1000000);
+$db = random_set(1000000);
+$la = array_keys($da);
+$lb = array_keys($db);
+
+timing('dict_diff', function () use ($da, $db) {
+    return array_diff_key($da, $db);
+});
+
+timing('list_diff', function () use ($la, $lb) {
+    return array_diff($la, $lb);
+});
+
+timing('dict_intersect', function () use ($da, $db) {
+    return array_intersect_key($da, $db);
+});
+
+timing('list_intersect', function () use ($la, $lb) {
+    return array_intersect($la, $lb);
+});
+
+timing('dict_merge', function () use ($da, $db) {
+    return array_merge($da, $db);
+});
+
+timing('list_merge', function () use ($la, $lb) {
+    return array_merge($la, $lb);
+});
+
+timing('dict_save', function () use ($da, $db) {
+    save_data(__DIR__ . '/da.txt', $da);
+    save_data(__DIR__ . '/db.txt', $db);
+});
+
+timing('list_save', function () use ($la, $lb) {
+    save_data(__DIR__ . '/la.txt', $la);
+    save_data(__DIR__ . '/lb.txt', $lb);
+});
+
+timing('dict_load', function () use ($da, $db) {
+    load_data(__DIR__ . '/da.txt');
+    load_data(__DIR__ . '/db.txt');
+});
+
+timing('list_load', function () use ($la, $lb) {
+    load_data(__DIR__ . '/la.txt');
+    load_data(__DIR__ . '/lb.txt');
+});
