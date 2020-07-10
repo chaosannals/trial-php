acd4025808389c4588d319af320b7e2f98a08f48
chaosannals <chaosannals@outlook.com>
2020-07-10 19:58:19 +0800

    pack

--- pack/vs_split.php
+++ pack/vs_split.php
@@ -1,2 +1,9 @@
 <?php
 
+// 大端
+$a = pack('nNn', 0xA00A, 123456, 0x0AA0);
+$c = pack('nNn', 0xA11A, 5145132, 0x1AA1);
+$b = unpack("na/Nb/nc", "$a $c");
+$d = unpack("na/Nb/nc", "$a $c", 9);
+var_export($b);
+var_export($d);
