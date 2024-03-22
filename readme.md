f041fb456f55549dae2a4fbf19af67f70c0a988d
chaosannals <chaosannals@outlook.com>
2024-03-22 15:58:36 +0800

    save

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # [exert-swoole](https://github.com/chaosannals/exert-swoole)
+
+注：git 拉去后 文件 launch 的 CRLF 会导致不能执行，要改成 LF 才能运行。
--- tcpsrv/app.php
+++ tcpsrv/app.php
@@ -2,11 +2,15 @@
 
 $server = new Swoole\Server("0.0.0.0", 9503);
 
+file_put_contents('php://stdout', "start tcp server.");
+
 $server->on('connect', function ($server, $fd){
     echo "connection open: {$fd}\n";
 });
 
 $server->on('receive', function ($server, $fd, $reactor_id, $data) {
+    $info = $server->getClientInfo($fd);
+    file_put_contents('php://stdout', var_export($info,true));
     $server->send($fd, "Swoole: {$data}");
     $server->close($fd);
 });
